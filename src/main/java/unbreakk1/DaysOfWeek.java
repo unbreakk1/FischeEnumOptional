6a89dd155d2e16bb0a13a9a55b8fb0ecaebf9487
unbreakk1 <95477818+unbreakk1@users.noreply.github.com>
2025-02-12 10:47:54 +0100

    Initial commit

---
+++ src/main/java/unbreakk1/DaysOfWeek.java
@@ -0,0 +1,22 @@
+package unbreakk1;
+
+public enum DaysOfWeek
+{
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY;
+
+    public static String getDayType(DaysOfWeek day) {
+        return switch (day)
+        {
+            case SATURDAY, SUNDAY -> "Weekend";
+            default -> day.name();
+        };
+    }
+}
+
+
---
+++ src/main/java/unbreakk1/Main.java
@@ -0,0 +1,11 @@
+package unbreakk1;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println(DaysOfWeek.getDayType(DaysOfWeek.MONDAY)); // Output: MONDAY
+        System.out.println(DaysOfWeek.getDayType(DaysOfWeek.SATURDAY)); // Output: Weekend
+
+    }
+}
